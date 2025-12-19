b7156df47225f70becbc5825d0eb95465a1f3a90
Johnny Vineall <vineall@live.co.uk>
2025-12-19 12:40:15 +0000

    Terraform now using kubectl

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
+      version = "~> 4.0"
     }
   }
 
@@ -63,12 +63,12 @@ resource "null_resource" "docker_push" {
   depends_on = [azurerm_container_registry.acr]
 
   triggers = {
-    image_tag = "v1.0.2" 
+    image_tag = "v1.0.3"
   }
 
   provisioner "local-exec" {
     # Move up one level from 'terraform' folder, then into 'src/azure-sa'
-    working_dir = "${path.module}/../src/azure-sa" 
+    working_dir = "${path.module}/../src/azure-sa"
 
     command = <<EOT
       az acr login --name ${azurerm_container_registry.acr.name}
@@ -76,6 +76,41 @@ resource "null_resource" "docker_push" {
       docker push ${azurerm_container_registry.acr.login_server}/filevault-app:${self.triggers.image_tag}
     EOT
   }
+}
+
+# 6. Kubernetes cluster
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "filevault-aks-v3"
+  location            = "northeurope"
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "filevault--FilevaultJohnnyT-348549"
+
+  default_node_pool {
+    name       = "nodepool1"
+    node_count = 1
+    vm_size    = "Standard_B2s_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  linux_profile {
+    admin_username = "azureuser"
+    ssh_key{
+      key_data = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDK4HHnI1gBsfPwny0A6Dq6BF4toiEpZt8EcqojjMdJY4J99WkiBtZGB/u/4aY6f4xbWKGc+P8GGK7rc/Las5NGT+9GXPt2JVEeFj0EAmJa+dFnRT1FqUAAOrhqpcdb5GLa4sMeZf5SiSa0h9Y3cMzyli6dr3Y/HJrxZDU1ZMQS0txaYqCAadN9taqK9uzrZiC93SEhane+91+0IKFc6+nxYWDYh3h0+/KhxOzKU4Arsx5jx8o+eGNMe3ctTS90C15O2sxYSlTBs/2ZN487bgUkuJF5B1KxyWRK1tylWcL1YbWzjT7x8TMtFIwfBLiEOYB9tsBWB0F735bg/4D7Oatf"
+    }
+  }
+
+  role_based_access_control_enabled = true
+}
+
+resource "azurerm_role_assignment" "aks_to_acr" {
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.acr.id
+  skip_service_principal_aad_check = true
 }
 
 # 6. Container instance (app)
