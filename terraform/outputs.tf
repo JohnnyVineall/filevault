58ce84817beee145d89c6fc482e4e68fd6164d68
Johnny Vineall <johnny.vineall@HMCTS.NET>
2025-12-22 10:45:19 +0000

    Creating CI-CD pipeline (#5)

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -10,3 +10,16 @@ output "storage_primary_key" {
 # output "app_url" {
 #   value = "http://${azurerm_container_group.app.fqdn}:3000"
 # }
+
+output "storage_account_name" {
+  value = azurerm_storage_account.storage.name
+}
+
+output "storage_account_key" {
+  value     = azurerm_storage_account.storage.primary_access_key
+  sensitive = true
+}
+
+output "container_name" {
+  value = azurerm_storage_container.container.name
+}
