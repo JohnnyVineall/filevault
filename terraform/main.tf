aaa84e3cd7d6f8f79baadb1325367a5ffb493c8c
Johnny Vineall <vineall@live.co.uk>
2025-12-23 10:44:16 +0000

    Implementing auto scaling

--- terraform/main.tf
+++ terraform/main.tf
@@ -65,26 +65,6 @@ resource "azurerm_container_registry" "acr" {
   admin_enabled       = true
 }
 
-# Local deployment automation stuff
-# resource "null_resource" "docker_push" {
-#   depends_on = [azurerm_container_registry.acr]
-
-#   triggers = {
-#     image_tag = "v1.0.3"
-#   }
-
-  # provisioner "local-exec" {
-  #   # Move up one level from 'terraform' folder, then into 'src/azure-sa'
-  #   working_dir = "${path.module}/../src/azure-sa"
-
-  #   command = <<EOT
-  #     az acr login --name ${azurerm_container_registry.acr.name}
-  #     docker build --platform linux/amd64 -t ${azurerm_container_registry.acr.login_server}/filevault-app:${self.triggers.image_tag} .
-  #     docker push ${azurerm_container_registry.acr.login_server}/filevault-app:${self.triggers.image_tag}
-  #   EOT
-  # }
-# }
-
 # 6. Kubernetes cluster
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "filevault-aks-v3"
@@ -92,10 +72,26 @@ resource "azurerm_kubernetes_cluster" "aks" {
   resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = "filevault--FilevaultJohnnyT-348549"
 
+  # default_node_pool {
+  #   name       = "nodepool1"
+  #   node_count = 1
+  #   vm_size    = "Standard_B2s_v2"
+  # }
+
   default_node_pool {
-    name       = "nodepool1"
-    node_count = 1
-    vm_size    = "Standard_B2s_v2"
+    name                = "nodepool1"
+    vm_size             = "Standard_B2s_v2"
+    
+    auto_scaling_enabled = true
+    
+    min_count            = 1
+    max_count            = 3
+
+    node_count           = 1 
+
+    upgrade_settings {
+      max_surge = "10%"
+    }
   }
 
   identity {
@@ -119,38 +115,3 @@ resource "azurerm_role_assignment" "aks_to_acr" {
   skip_service_principal_aad_check = true
 }
 
-# 6. Container instance (app)
-# resource "azurerm_container_group" "app" {
-#   name                = "filevault-app-johnny"
-#   location            = azurerm_resource_group.rg.location
-#   resource_group_name = azurerm_resource_group.rg.name
-#   ip_address_type     = "Public"
-#   dns_name_label      = "filevault-app-johnny"
-#   os_type             = "Linux"
-
-#   depends_on = [null_resource.docker_push, azurerm_storage_container.container]
-
-#   container {
-#     name   = "filevault-app-johnny"
-#     image  = "${azurerm_container_registry.acr.login_server}/filevault-app:${null_resource.docker_push.triggers.image_tag}"
-#     cpu    = "1"
-#     memory = "1.5"
-
-#     ports {
-#       port     = 3000
-#       protocol = "TCP"
-#     }
-
-#     secure_environment_variables = {
-#       "AZURE_STORAGE_ACCOUNT_NAME" = azurerm_storage_account.storage.name
-#       "AZURE_STORAGE_ACCOUNT_KEY"  = azurerm_storage_account.storage.primary_access_key
-#       "AZURE_STORAGE_CONTAINER_NAME" = azurerm_storage_container.container.name
-#     }
-
-#   }
-#   image_registry_credential {
-#     server   = azurerm_container_registry.acr.login_server
-#     username = azurerm_container_registry.acr.admin_username
-#     password = azurerm_container_registry.acr.admin_password
-#   }
-# }
